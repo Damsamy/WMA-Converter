363444f444a8fd02dee0c774ec44ebcef993fa51
SamyD <80007439+Damsamy@users.noreply.github.com>
2024-02-27 22:10:34 -0300

    Update wmaFinder.py

--- wmaFinder.py
+++ wmaFinder.py
@@ -8,19 +8,5 @@ def find_wma(folder):
 
     entries = glob.glob(query, root_dir=f"{folder}", recursive=True)
     entries_num = len(entries)
-    # if entries_num == 0:
-        # print(f"no results for query: {query}")
-    # else:
-        # print(f"Found {entries_num} result(s) for query: {query}")
-
-    # print("-" * 10)
-
-    # for entry in entries:
-        # print(entry)
 
     return entries
-
-
-
-
-# find_wma(folder="/home/spiced/Music/Music/Music/Treyarch Sound/Call of Duty_ Black Ops/")
