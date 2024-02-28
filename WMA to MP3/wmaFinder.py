f6d5de9290b4802366175c97dd4016635801f3ff
SamyD <80007439+Damsamy@users.noreply.github.com>
2024-02-27 22:15:51 -0300

    Add files via upload

---
+++ WMA to MP3/wmaFinder.py
@@ -0,0 +1,16 @@
+import glob
+
+
+def find_wma(folder):
+
+    # this variable defines which file name and extension will be searched
+    query = "**/*.wma"
+
+    entries = glob.glob(query, root_dir=f"{folder}", recursive=True)
+    entries_num = len(entries)
+
+    return entries
+
+
+f = find_wma('/home/spiced/Downloads/System Of A Down/')
+print(f)
